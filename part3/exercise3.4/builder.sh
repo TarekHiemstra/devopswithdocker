4fc0d98c3f33077154192a1395f635897847adf7
Tarek Hiemstra <tarek.hiemstra@gmail.com>
2024-10-14 18:47:57 +0300

    Add exercise 3.4

---
+++ part3/exercise3.4/builder.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# EXERCISE 3.4: BUILDING IMAGES FROM INSIDE OF A CONTAINER
+
+if [[ $# -ne 2 ]]; then
+  echo ""
+  echo "This script requires 2 arguments and clones a GitHub repo, builds the Dockerfile and pushes it to Docker Hub."
+  echo "Usage: ./builder.sh <GitHub repository> <Docker Hub repository>"
+  echo "For example: ./builder.sh docker-hy/docker-hy.github.io tarekhiemstra/docker-hy"
+  echo "IMPORTANT: The GitHub repo must have a Dockerfile and the Docker Hub repo must be created beforehand."
+  echo ""
+  exit 0;
+fi
+
+git clone git@github.com:$1.git || git clone https://github.com/$1.git || exit 1
+cd "$(basename "$1")" || exit 1
+docker build . -t $2 || sudo docker build . -t $2 || exit 1
+docker login -u $DOCKER_USER -p $DOCKER_PWD || exit 1
+docker push $2 || exit 1
